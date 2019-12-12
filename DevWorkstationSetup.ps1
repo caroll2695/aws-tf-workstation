c417af69c145a4a1dab07d3ec3cae347ae527f47
caroll2695 <caroll2695@gmail.com>
2019-12-12 06:20:11 +0000

    aws workstation code

---
+++ DevWorkstationSetup.ps1
@@ -0,0 +1,12 @@
+﻿# Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco feature enable -n allowGlobalConfirmation                 # bypass confirmations
+choco install terraform -Force
+choco install vscode -Force
+code --install-extension ms-vscode-remote.remote-ssh # Add a VSC Plug in for remote connection to ubuntu machine
+choco install git.install -Force
+choco install googlechrome –Force
+
+choco install firefox
+choco install awscli
