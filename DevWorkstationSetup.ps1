86bed9beb0f9f4dc6c9780f3e18e93ae5fc72fd2
Caroll George <50725768+caroll2695@users.noreply.github.com>
2019-12-13 09:37:59 -0600

    Update DevWorkstationSetup.ps1
    updating ps script

--- DevWorkstationSetup.ps1
+++ DevWorkstationSetup.ps1
@@ -1,12 +1,15 @@
-﻿# Set-ExecutionPolicy AllSigned
+# Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco feature enable -n allowGlobalConfirmation                 # bypass confirmations
 choco install terraform -Force
 choco install vscode -Force
 code --install-extension ms-vscode-remote.remote-ssh # Add a VSC Plug in for remote connection to ubuntu machine
+code --install-extension terraform
+code --install-extension 
 choco install git.install -Force
 choco install googlechrome –Force
 
-choco install firefox
-choco install awscli
+choco install firefox –Force
+choco install awscli –Force
+choco install notepadplusplus -Force
